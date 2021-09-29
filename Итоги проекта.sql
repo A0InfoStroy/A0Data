6949f8251ca10222caf788de9ab67c8a6091d951
Vladimir Butov <vbutov@infostroy.ru>
2021-09-29 13:01:50 +0300

    -

--- Итоги проекта.sql
+++ Итоги проекта.sql
@@ -1,17 +1,17 @@
-SELECT 
-  [A0Proj].Mark AS [Шифр проекта],
-  [SSRTotal].Estimate_S AS [ССР Сметная стоимость ССР(Проекта) строительные], 
-  [SSRTotal].Estimate_M AS [ССР Сметная стоимость ССР(Проекта) монтажные], 
-  [SSRTotal].Estimate_E AS [ССР Сметная стоимость ССР(Проекта) оборудование], 
-  [SSRTotal].Estimate_O AS [ССР Сметная стоимость ССР(Проекта) прочие]
+п»їSELECT 
+  [A0Proj].Mark AS [РЁРёС„СЂ РїСЂРѕРµРєС‚Р°],
+  [SSRTotal].Estimate_S AS [РЎРЎР  РЎРјРµС‚РЅР°СЏ СЃС‚РѕРёРјРѕСЃС‚СЊ РЎРЎР (РџСЂРѕРµРєС‚Р°) СЃС‚СЂРѕРёС‚РµР»СЊРЅС‹Рµ], 
+  [SSRTotal].Estimate_M AS [РЎРЎР  РЎРјРµС‚РЅР°СЏ СЃС‚РѕРёРјРѕСЃС‚СЊ РЎРЎР (РџСЂРѕРµРєС‚Р°) РјРѕРЅС‚Р°Р¶РЅС‹Рµ], 
+  [SSRTotal].Estimate_E AS [РЎРЎР  РЎРјРµС‚РЅР°СЏ СЃС‚РѕРёРјРѕСЃС‚СЊ РЎРЎР (РџСЂРѕРµРєС‚Р°) РѕР±РѕСЂСѓРґРѕРІР°РЅРёРµ], 
+  [SSRTotal].Estimate_O AS [РЎРЎР  РЎРјРµС‚РЅР°СЏ СЃС‚РѕРёРјРѕСЃС‚СЊ РЎРЎР (РџСЂРѕРµРєС‚Р°) РїСЂРѕС‡РёРµ]
 FROM [A0Proj]
-	-- Итоги по составляющим
+	-- РС‚РѕРіРё РїРѕ СЃРѕСЃС‚Р°РІР»СЏСЋС‰РёРј
 	LEFT JOIN [SSRTotal] ON SSRTotal.ProjID = [A0Proj].ProjID 
 		AND [A0Proj].TotalID = [A0Proj].TotalID
-	-- Порядок Проектов
+	-- РџРѕСЂСЏРґРѕРє РџСЂРѕРµРєС‚РѕРІ
 	LEFT JOIN [ProjOrder] ON [A0Proj].ProjID = [ProjOrder].ProjID 
-WHERE [A0Proj].IncludeInTotals = 1 -- Включать Проект в итоги комплекса (0, 1)
-  AND [A0Proj].ProjId = 221 -- ИД проекта
-  --AND [A0Proj].GUID =  -- GUID проекта
+WHERE [A0Proj].IncludeInTotals = 1 -- Р’РєР»СЋС‡Р°С‚СЊ РџСЂРѕРµРєС‚ РІ РёС‚РѕРіРё РєРѕРјРїР»РµРєСЃР° (0, 1)
+  AND [A0Proj].ProjId = 221 -- РР” РїСЂРѕРµРєС‚Р°
+  --AND [A0Proj].GUID =  -- GUID РїСЂРѕРµРєС‚Р°
 
 
