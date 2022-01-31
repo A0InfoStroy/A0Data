c2242ac7dbf7d3b3480c7ef4c5e3b399a675ca54
Vladimir Butov <vbutov@infostroy.ru>
2022-01-31 19:20:10 +0300

    Итоги строк ЛС и Актов

---
+++ Итоги строк Актов.sql
@@ -0,0 +1,29 @@
+﻿SELECT
+  [A0Proj].Mark AS [Шифр проекта],
+  [OSTitle].Shifr AS [Шфир ОС],
+  [LSTitle].Shifr AS [Шифр ЛС], 
+  [ActTitle].Shifr AS [Шифр Акт], 
+  [ActStringTitle].Basing AS [Обоснование строки Акта],
+  [ActStringTitle].LSName AS [Наименование строки Акта],
+  [ActStrTotal].PZ
+FROM [LSStringTitle] AS [ActStringTitle] 
+   -- Заголовок акта
+    JOIN [LSTitle] AS [ActTitle] ON [ActTitle].ProjID = [ActStringTitle].ProjID 
+	  AND [ActTitle].LSTitleID = [ActStringTitle].LSTitleID
+	-- Итоги строки акта
+	JOIN [LSStrTotal] AS [ActStrTotal] ON [ActStringTitle].ProjID = [ActStrTotal].ProjID 
+	  AND [ActStringTitle].LSTitleID = [ActStrTotal].LSTitleID 
+	  AND [ActStringTitle].LSStrTotalID = [ActStrTotal].LSTID
+	-- ЛС
+	JOIN [LSTitle] ON [LSTitle].ProjID = [ActTitle].ProjID
+		AND [LSTitle].OSID = [ActTitle].OSID
+		AND [LSTitle].LSTitleID = [ActTitle].ActLSID
+	-- ОС
+	JOIN [OSTitle] ON [LSTitle].ProjID = [OSTitle].ProjID 
+		AND [LSTitle].OSID = [OSTitle].OSTitleID	
+	-- Проект
+	JOIN [A0Proj] ON [LSTitle].ProjID = [A0Proj].ProjID
+WHERE [A0Proj].ProjId = 253 -- ИД проекта
+	AND [ActTitle].ActLSID > 0 -- Только акты
+
+
---
+++ Итоги строк ЛС.sql
@@ -0,0 +1,23 @@
+﻿SELECT 
+  [A0Proj].Mark AS [Шифр проекта],
+  [OSTitle].Shifr AS [Шфир ОС],
+  [LSTitle].Shifr AS [Шифр ЛС], 
+  [LSStringTitle].Basing AS [Обоснование строки ЛС],
+  [LSStringTitle].LSName AS [Наименование строки ЛС],
+  [LSStrTotal].PZ
+FROM [LSStringTitle]
+	-- Итоги строки ЛС
+	JOIN [LSStrTotal] ON [LSStringTitle].ProjID = [LSStrTotal].ProjID 
+	  AND [LSStringTitle].LSTitleID = [LSStrTotal].LSTitleID 
+	  AND [LSStringTitle].LSStrTotalID = [LSStrTotal].LSTID
+	-- ЛС
+	JOIN [LSTitle] ON [LSTitle].ProjID = [LSStringTitle].ProjID
+		AND [LSTitle].LSTitleID = [LSStringTitle].LSTitleID
+	-- ОС
+	JOIN [OSTitle] ON [LSTitle].ProjID = [OSTitle].ProjID 
+		AND [LSTitle].OSID = [OSTitle].OSTitleID	
+	-- Проект
+	JOIN [A0Proj] ON [LSTitle].ProjID = [A0Proj].ProjID
+WHERE [A0Proj].ProjId = 253 -- ИД проекта
+  AND [LSTitle].ActLSID = 0 -- Только ЛС
+
